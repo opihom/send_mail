55765175952e9237d1af8ecb493e34f2a96610e5
opihom <n.bekaert@gmail.com>
2025-04-15 09:55:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import datetime
 
 app = Flask(__name__)
 
-@app.route('/pixel.gif')
-def pixel():
+GOOGLE_SHEETS_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbxjAi_l-Kpj8rHHvwnIN32DZfgTMaq0_G7vQgiHR4ttlGJZaY_DkNCglRVQmLozL6jpOg/exec"
+
+@app.route('/pixel_log.gif')
+def pixel_log():
     # Pixel GIF transparent 1x1 (en bytes)
     pixel_bytes = (
         b'GIF89a\x01\x00\x01\x00\x80\x00\x00\x00\x00\x00'
@@ -19,6 +21,27 @@ def pixel():
     print(f"[{timestamp}] Pixel chargé pour UID={uid}, IP={ip}")
     
     return Response(pixel_bytes, mimetype='image/gif')
+
+@app.route('/pixel.gif')
+def pixel():
+pixel_bytes = (
+        b'GIF89a\x01\x00\x01\x00\x80\x00\x00\x00\x00\x00'
+        b'\xFF\xFF\xFF!\xF9\x04\x01\x00\x00\x00\x00,\x00\x00'
+        b'\x00\x00\x01\x00\x01\x00\x00\x02\x02L\x01\x00;'
+    )
+
+uid = request.args.get('uid', 'inconnu')
+ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+# Envoie vers Google Sheets
+try:
+	requests.post(GOOGLE_SHEETS_WEBHOOK_URL, json={
+		"uid": uid,
+		"ip": ip
+	})
+except Exception as e:
+	print("Erreur envoi Google Sheets:", e)
+	
+return Response(pixel_bytes, mimetype='image/gif')
 
 @app.route('/')
 def home():
