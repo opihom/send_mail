7890759e021bc48e70f83813a9b0cbb0194eb37b
opihom <n.bekaert@gmail.com>
2025-04-15 09:59:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,24 +24,25 @@ def pixel_log():
 
 @app.route('/pixel.gif')
 def pixel():
-pixel_bytes = (
+    pixel_bytes = (
         b'GIF89a\x01\x00\x01\x00\x80\x00\x00\x00\x00\x00'
         b'\xFF\xFF\xFF!\xF9\x04\x01\x00\x00\x00\x00,\x00\x00'
         b'\x00\x00\x01\x00\x01\x00\x00\x02\x02L\x01\x00;'
     )
 
-uid = request.args.get('uid', 'inconnu')
-ip = request.headers.get('X-Forwarded-For', request.remote_addr)
-# Envoie vers Google Sheets
-try:
-	requests.post(GOOGLE_SHEETS_WEBHOOK_URL, json={
-		"uid": uid,
-		"ip": ip
-	})
-except Exception as e:
-	print("Erreur envoi Google Sheets:", e)
-	
-return Response(pixel_bytes, mimetype='image/gif')
+    uid = request.args.get('uid', 'inconnu')
+    ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+
+    # Envoie vers Google Sheets
+    try:
+        requests.post(GOOGLE_SHEETS_WEBHOOK_URL, json={
+            "uid": uid,
+            "ip": ip
+        })
+    except Exception as e:
+        print("Erreur envoi Google Sheets:", e)
+
+    return Response(pixel_bytes, mimetype='image/gif')
 
 @app.route('/')
 def home():
